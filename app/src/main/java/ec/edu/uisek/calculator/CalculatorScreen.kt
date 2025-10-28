d5c1a7d86c452e2ad675f399c7305ea8e03213ce
Esteban Quijia <eaquijia@puce.edu.ec>
2025-10-28 15:50:47 -0500

    2do commit Calculator Screen 1

--- app/src/main/java/ec/edu/uisek/calculator/CalculatorScreen.kt
+++ app/src/main/java/ec/edu/uisek/calculator/CalculatorScreen.kt
@@ -1,22 +1,32 @@
 package ec.edu.uisek.calculator
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,6 +34,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import ec.edu.uisek.calculator.ui.theme.Purple40
+import ec.edu.uisek.calculator.ui.theme.Purple80
 import ec.edu.uisek.calculator.ui.theme.Red
 import ec.edu.uisek.calculator.ui.theme.UiSekBlue
 
@@ -32,7 +43,6 @@ fun CalculatorScreen(
     viewModel: CalculatorViewModel = viewModel()
 ) {
     val state = viewModel.state
-
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -44,15 +54,15 @@ fun CalculatorScreen(
             text = state.display,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 16.dp),
-            fontSize = 56.sp,
+                .padding(16.dp),
             textAlign = TextAlign.End,
+            fontSize = 56.sp,
             color = Color.White,
             fontWeight = FontWeight.Bold,
-            maxLines = 1
+            maxLines =  1
         )
 
-        CalculatorGrid(onEvent = viewModel::onEvent)
+        CalculatorGrid ( onEvent = viewModel :: onEvent)
     }
 }
 
@@ -64,7 +74,6 @@ fun CalculatorGrid(onEvent: (CalculatorEvent) -> Unit) {
         "1", "2", "3", "−",
         "0", ".", "=", "+"
     )
-
     LazyVerticalGrid(
         columns = GridCells.Fixed(4),
         modifier = Modifier
@@ -76,21 +85,20 @@ fun CalculatorGrid(onEvent: (CalculatorEvent) -> Unit) {
         items(buttons.size) { index ->
             val label = buttons[index]
             CalculatorButton(label = label) {
-                when (label) {
-                    in "0".."9" -> onEvent(CalculatorEvent.Number(label))
+                when(label) {
+                    in "0" .. "9" -> onEvent(CalculatorEvent.Number(label))
                     "." -> onEvent(CalculatorEvent.Decimal)
                     "=" -> onEvent(CalculatorEvent.Calculate)
                     else -> onEvent(CalculatorEvent.Operator(label))
                 }
             }
         }
-
-        item(span = { GridItemSpan(2) }) {
+        item (span = { GridItemSpan(currentLineSpan = 2) } ){
             CalculatorButton(label = "AC") {
                 onEvent(CalculatorEvent.AllClear)
             }
         }
-
+        item {}
         item {
             CalculatorButton(label = "C") {
                 onEvent(CalculatorEvent.Clear)
@@ -99,23 +107,24 @@ fun CalculatorGrid(onEvent: (CalculatorEvent) -> Unit) {
     }
 }
 
+
 @Composable
 fun CalculatorButton(label: String, onClick: () -> Unit) {
-    Box(
+    Box (
         modifier = Modifier
             .aspectRatio(if (label == "AC") 2f else 1f)
             .fillMaxSize()
             .clip(CircleShape)
             .background(
                 when (label) {
-                    in listOf("÷", "×", "−", "+", "=") -> Purple40
+                    in listOf("÷", "×", "−", "+", "=", ".") -> Purple40
                     in listOf("AC", "C") -> Red
                     else -> UiSekBlue
                 }
             )
             .clickable { onClick() },
         contentAlignment = Alignment.Center
-    ) {
+    ){
         Text(
             text = label,
             color = Color.White,
